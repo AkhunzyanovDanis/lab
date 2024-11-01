c1db909ca004999f640ebf010da652c992180ef9
AkhunzyanovDanis <akhunzyanovdanis2005@gmail.com>
2024-11-01 21:52:49 +0300

    Update №2.py
    Можно ли во втором задании написать round(m) + 1

--- Лабораторная работа №2/№2.py
+++ Лабораторная работа №2/№2.py
@@ -6,9 +6,10 @@ increase = 0.03  # Ежемесячный рост цен
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 expenses = 0
 earnings = 0
-for i in range(months):
+for _ in range(months):
     earnings = earnings + salary
     expenses = expenses + spend
-    spend = spend * (increase+1)
+    spend = spend * (increase + 1)
 m = expenses - earnings
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(m))
+import math
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", math.ceil(m))
