58a9a11b768dad16070e776332e9866d08ca4c6c
AkhunzyanovDanis <akhunzyanovdanis2005@gmail.com>
2024-11-20 10:28:39 +0300

    Update task1.py

--- Лабораторная работа №1/task1.py
+++ Лабораторная работа №1/task1.py
@@ -2,7 +2,9 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 # TODO заменить значение пропущенного элемента средним арифметическим
 sum_numbers = sum(filter(None, numbers))
-k = len(numbers)
-sr = sum_numbers/k
-numbers[4] = sr
+# количество символов
+numbers_of_numbers = len(numbers)
+# среднее значение
+average = sum_numbers/numbers_of_numbers
+numbers[4] = average
 print("Измененный список:", numbers)
