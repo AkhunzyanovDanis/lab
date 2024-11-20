14c4a79a2fc6a8de8891219f0261266dfd0f7e36
AkhunzyanovDanis <akhunzyanovdanis2005@gmail.com>
2024-11-20 10:48:15 +0300

    Update task1.py

--- Лабораторная работа №4/task1.py
+++ Лабораторная работа №4/task1.py
@@ -1,19 +1,15 @@
 # TODO решите задачу
 import json
 
-def task() -> float:
 
+def task(file_input):
     sum_ = 0
-    with open("input.json", "r", encoding="utf-8") as file_r:
+    with open("file_input", "r", encoding="utf-8") as file_r:
         data = json.load(file_r)
-
         for item in data:
-           number1 = item.get("score", 0)
-           number2 = item.get("weight", 0)
 
-           if number1 and number2:
-            sum_ += number1 * number2
+            sum_ += item.get("score", 0) * item.get("weight", 0)
+        return round(sum_, 3)
 
-    return round(sum_, 3)
 
-print(task())
+print(task(r'input.json'))
